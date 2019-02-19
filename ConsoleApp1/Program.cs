069fbb79b7a0f1ce7a6527efa93d1dcdff38696b
自由之翼 <@LAPTOP-0QVJ42G4>
2019-02-19 12:03:25 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            哈哈哈
+        }
+    }
+}
